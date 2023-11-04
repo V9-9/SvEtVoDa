e054d53a8353d04ea109b5b6d4932a7f20928ea8
V9-9 <148236657+V9-9@users.noreply.github.com>
2023-11-04 22:11:53 +0300

    SV2

--- app/src/main/java/com/example/svetvoda/MainActivity.java
+++ app/src/main/java/com/example/svetvoda/MainActivity.java
@@ -1,5 +1,6 @@
 package com.example.svetvoda;
 
+import android.content.SharedPreferences;
 import android.widget.Toast;
 import android.content.ClipData;
 import android.content.ClipboardManager;
@@ -17,14 +18,8 @@ public class MainActivity extends AppCompatActivity {
     private TextView resultText;
     private Button calculateButton;
 
-    public void copyText(View view) {
-        String textToCopy = resultText.getText().toString();
-        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
-        ClipData clip = ClipData.newPlainText("label", textToCopy);
-        clipboard.setPrimaryClip(clip);
-        Toast.makeText(this, "Текст скопирован", Toast.LENGTH_SHORT).show();
-    }
-
+    private SharedPreferences sharedPreferences;
+    private static final String PREFS_NAME = "MyPrefsFile";
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,6 +33,8 @@ public class MainActivity extends AppCompatActivity {
         resultText = findViewById(R.id.resultText);
         calculateButton = findViewById(R.id.calculateButton);
 
+        restoreInputState();
+
         calculateButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -49,7 +46,47 @@ public class MainActivity extends AppCompatActivity {
                 String resultMessage = String.format("Илья, привет!\nВода=%.2f руб.;\nЭлектричество=%.2f руб.;\nИТОГО: %.2f руб.",
                         ((a - 307.93)+(b + c)), d, ((a - 307.93) + (b + c) + d));
                 resultText.setText(resultMessage);
+
+                saveInputState();
             }
         });
+        Button clearButton = findViewById(R.id.clearButton);
+        clearButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                inputA.setText("");
+                inputB.setText("");
+                inputC.setText("");
+                inputD.setText("");
+
+                saveInputState();
+            }
+        });
+    }
+
+    private void saveInputState() {
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        SharedPreferences.Editor editor = sharedPreferences.edit();
+        editor.putString("inputA", inputA.getText().toString());
+        editor.putString("inputB", inputB.getText().toString());
+        editor.putString("inputC", inputC.getText().toString());
+        editor.putString("inputD", inputD.getText().toString());
+        editor.apply();
+    }
+
+    private void restoreInputState() {
+        sharedPreferences = getSharedPreferences(PREFS_NAME, MODE_PRIVATE);
+        inputA.setText(sharedPreferences.getString("inputA", ""));
+        inputB.setText(sharedPreferences.getString("inputB", ""));
+        inputC.setText(sharedPreferences.getString("inputC", ""));
+        inputD.setText(sharedPreferences.getString("inputD", ""));
+    }
+
+    public void copyText(View view) {
+        String textToCopy = resultText.getText().toString();
+        ClipboardManager clipboard = (ClipboardManager) getSystemService(Context.CLIPBOARD_SERVICE);
+        ClipData clip = ClipData.newPlainText("label", textToCopy);
+        clipboard.setPrimaryClip(clip);
+        Toast.makeText(this, "Текст скопирован", Toast.LENGTH_SHORT).show();
     }
 }
